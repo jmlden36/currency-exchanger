028f7aaa3a3178964e00ecf495077e5039c4c37c
Matt Luker <jamesmattluker@gmail.com>
2021-11-19 16:05:32 -0700

    add additional radio buttons for more currencies

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ function getElements(response, countryCode, dollarAmount) {
   if (response.conversion_rates) {     
     console.log("how");
     $('.show-conversion-rate').text(`The conversion rate is ${conversionRate}`); 
-    $('.show-conversion-amount').text(`The converted dollar amount is $${convertedDollars}`)   
+    $('.show-conversion-amount').text(`The converted dollar amount is $${convertedDollars}`);   
   } else {
     $('.showErrors').text(`There was an error: ${response.message}`);
   }
@@ -32,7 +32,7 @@ function getElements(response, countryCode, dollarAmount) {
 
 $(document).ready(function () {
   $('#currency-conversion').submit(function (event) {
-    let countryCodeInput = $("input:radio[name=countryChoice]:checked").val();
+    let countryCodeInput = $("input:radio[name=countryChoice]:checked").val();           
     let dollarAmountInput = parseInt($('#usd-amount').val());
     console.log(countryCodeInput);
     event.preventDefault();    
