a9f2b0f33d6e4c2695d397b84c3e7787cc7e59ff
Matt Luker <jamesmattluker@gmail.com>
2021-11-19 11:25:39 -0700

    WIP:trying to make a working API call

--- src/services/currency-service.js
+++ src/services/currency-service.js
@@ -2,13 +2,14 @@ export default class CurrencyService {
   static getCurrency()  {
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
       .then(function(response)  {
+        console.log(response.json());
         if (!response.ok) {
           throw Error(response.statusText);
         }
-        return response.json();
+        return response.json();         
       })
       .catch(function(error)  {
         return Error(error);
-      });      
+      });         
   }
 }
