c1af4b812889743136987b6544690d5ccb181d59
Matt Luker <jamesmattluker@gmail.com>
2021-12-01 17:37:03 -0700

    remove the forced error in the API call in currency-service.js

--- src/services/currency-service.js
+++ src/services/currency-service.js
@@ -1,6 +1,6 @@
 export default class CurrencyService {
   static getCurrency()  {
-    return fetch(`https://v6.exch1angerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
       .then(function(response)  { 
         if(!response.ok && response.statusText === "") {    
           return response.json(); 
